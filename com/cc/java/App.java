96ce7ac7f7889162a954ebf7c1ac90613bcb680f
Trung <trung1994@outlook.de>
2024-05-22 11:16:15 +0200

    App.java

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,14 +3,15 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-
-       
-       
-       Cat cat1 = new Cat();
-       System.out.println(cat1);
-
+           
+        // Cat --> Referenztyp (Klasse aus dem Objekt entsteht)
+        // cat2 --> Referenzvariable (Adresse des Objekts im RAM)
+       Cat cat1 = new Cat(); // Instanziierung >> Objekt
+       System.out.println("Blick von außen: "+cat1); // Adresse des Objekts
+        System.out.println("-----------------------------------------");
        Cat cat2 = new Cat();
-       System.out.println(cat2);
+       cat2.sayHi();
+       System.out.println("Blick von außen: "+cat2);
        
         // System.out.println(new Cat());
         // System.out.println(new Cat());
